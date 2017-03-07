b89a58442cad9186786c633af7b92ee7d16c68c8
bud aminof <budaminof@gmail.com>
2017-03-07 13:59:30 -0700

    css loader, and extractTextPlugin added to webpack to handel css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 // this require will be handeled by JS itself and not webpack
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
   // relative path
@@ -12,6 +13,23 @@ const config = {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
   },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/
+      },
+      {
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader'
+        }),
+        test: /\.css$/
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
 };
 
 module.exports = config;
